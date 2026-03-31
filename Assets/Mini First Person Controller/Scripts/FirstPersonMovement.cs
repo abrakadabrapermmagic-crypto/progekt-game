414a356ea2d94b937504e24ac6d434d8d8e4c293
sashaeee533 <abrakadabra.perm.magic@gmail.com>
2026-03-31 12:30:12 +0500

    Add

---
+++ Assets/Mini First Person Controller/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public KeyCode runningKey = KeyCode.LeftShift;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+    void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        IsRunning = canRun && Input.GetKey(runningKey);
+
+        float targetSpeed = IsRunning ? runSpeed : speed;
+
+        if (speedOverrides.Count > 0)
+        {
+            targetSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        // Гравитация
+        if (controller.isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        velocity.y += Physics.gravity.y * Time.deltaTime;
+
+        controller.Move(move * targetSpeed * Time.deltaTime);
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
